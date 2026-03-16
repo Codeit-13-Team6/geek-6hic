4b9a2b4524ee4a7c5dbdb400358f4cf3ce26f6b6
제현 <jehyeon@jehyeon-ui-Macmini.local>
2026-03-16 15:05:39 +0900

    feat: page 테스트 코드제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { DetailCardCommon } from "@/components/common/DetailCardCommon";
-
 export default function Home() {
-  return (
-    <div className="bg-gray-600 p-[20px]">
-      <DetailCardCommon />
-    </div>
-  );
+  return ;
 }
