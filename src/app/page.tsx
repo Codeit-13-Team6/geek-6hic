853b6978002c3f99dd7a4f8f725331a1827463a9
parksanghyun <basira@naver.com>
2026-03-16 14:24:42 +0900

    design: 기존 캘린더에 공용버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { CalendarCommon } from "@/components/common/CalendarCommon";
 export default function Home() {
-  return;
+  return(
+    <>
+      <CalendarCommon mode="single" />
+    </>
+  );
 }
