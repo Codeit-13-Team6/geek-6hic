3bbf2fd9da5bec2d6d487bcefcccf34338143917
sang7085 <54009751+sang7085@users.noreply.github.com>
2026-03-16 11:24:33 +0900

    feat: 캘린더, 타임, 빈데이터컴포넌트 작업완료 (#22)

---
+++ src/components/common/CalendarCommon.tsx
@@ -0,0 +1,247 @@
+"use client"
+
+import * as React from "react"
+import {
+  DayPicker,
+  getDefaultClassNames,
+  type DayButton,
+  type Locale,
+} from "react-day-picker"
+import Image from "next/image"
+import ArrowLeft from "@/assets/icon/arrow/arrow-left.svg"
+import ArrowRight from "@/assets/icon/arrow/arrow-right.svg"
+import ArrowDown from "@/assets/icon/arrow/arrow-down.svg"
+import { ko } from "date-fns/locale"
+import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react"
+
+
+// 컴포넌트 사용시 프롭스로 mode="single" 필요
+function CalendarCommon({
+  className,
+  classNames,
+  showOutsideDays = true,
+  captionLayout = "label",
+  buttonVariant = "ghost",
+  locale,
+  formatters,
+  components,
+  ...props
+}: React.ComponentProps<typeof DayPicker> & {
+  buttonVariant?: React.ComponentProps<typeof Button>["variant"]
+}) {
+  const defaultClassNames = getDefaultClassNames()
+
+  return (
+    <div>
+      <DayPicker
+        showOutsideDays={showOutsideDays}
+        className={cn(
+          "absolute group/calendar bg-background p-6 shadow-[0_10px_10px_-5px_rgba(0,0,0,0.04)] border-gray-100 w-[300px] rounded-[12] border-box [--cell-radius:var(--radius-md)] [--cell-size:--spacing(7)] in-data-[slot=card-content]:bg-transparent in-data-[slot=popover-content]:bg-transparent",
+          String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
+          String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
+          className
+        )}
+        captionLayout={captionLayout}
+        locale={ko}
+        formatters={{
+          formatMonthDropdown: (date) =>
+            date.toLocaleString(locale?.code, { month: "short" }),
+          ...formatters,
+        }}
+        classNames={{
+          root: cn("w-fit", defaultClassNames.root),
+          months: cn(
+            "relative flex flex-col gap-4 md:flex-row",
+            defaultClassNames.months
+          ),
+          month: cn("flex w-full flex-col gap-4", defaultClassNames.month),
+          nav: cn(
+            "absolute inset-x-0 top-0 flex w-full items-center justify-between gap-1",
+            defaultClassNames.nav
+          ),
+          button_previous: cn(
+            buttonVariants({ variant: buttonVariant }),
+            "size-(--cell-size) cursor-pointer p-0 select-none aria-disabled:opacity-50",
+            defaultClassNames.button_previous
+          ),
+          button_next: cn(
+            buttonVariants({ variant: buttonVariant }),
+            "size-(--cell-size) p-0 cursor-pointer select-none aria-disabled:opacity-50",
+            defaultClassNames.button_next
+          ),
+          month_caption: cn(
+            "flex h-(--cell-size) w-full items-center justify-center px-(--cell-size)",
+            defaultClassNames.month_caption
+          ),
+          dropdowns: cn(
+            "flex h-(--cell-size) w-full items-center justify-center gap-1.5 text-sm font-medium",
+            defaultClassNames.dropdowns
+          ),
+          dropdown_root: cn(
+            "relative rounded-(--cell-radius)",
+            defaultClassNames.dropdown_root
+          ),
+          dropdown: cn(
+            "absolute inset-0 bg-popover opacity-0",
+            defaultClassNames.dropdown
+          ),
+          caption_label: cn(
+            "font-medium select-none",
+            captionLayout === "label"
+              ? "text-sm text-gray-800 font-semibold"
+              : "flex items-center gap-1 rounded-(--cell-radius) text-sm [&>svg]:size-3.5 [&>svg]:text-muted-foreground",
+            defaultClassNames.caption_label
+          ),
+          table: "w-full border-collapse",
+          weekdays: cn("flex", defaultClassNames.weekdays),
+          weekday: cn(
+            "flex-1 rounded-(--cell-radius) text-sm font-normal select-none font-semibold text-gray-600",
+            defaultClassNames.weekday
+          ),
+          week: cn("mt-2 flex w-full", defaultClassNames.week),
+          week_number_header: cn(
+            "w-(--cell-size) select-none",
+            defaultClassNames.week_number_header
+          ),
+          week_number: cn(
+            "text-[0.8rem] text-muted-foreground select-none",
+            defaultClassNames.week_number
+          ),
+          day: cn(
+            "group/day relative aspect-square h-full rounded-(--cell-radius) text-center select-none [&:last-child[data-selected=true]_button]:rounded-r-(--cell-radius)", 
+            props.showWeekNumber
+              ? "[&:nth-child(2)[data-selected=true]_button]:rounded-l-(--cell-radius)"
+              : "[&:first-child[data-selected=true]_button]:rounded-l-(--cell-radius)",
+            defaultClassNames.day
+          ),
+          range_start: cn(
+            "relative isolate z-0 rounded-l-(--cell-radius) bg-muted after:absolute after:inset-y-0 after:right-0 after:w-4 after:bg-muted",
+            defaultClassNames.range_start
+          ),
+          range_middle: cn("rounded-none", defaultClassNames.range_middle),
+          range_end: cn(
+            "relative isolate z-0 rounded-r-(--cell-radius) bg-muted after:absolute after:inset-y-0 after:left-0 after:w-4 after:bg-muted",
+            defaultClassNames.range_end
+          ),
+          today: cn(
+            "rounded-(--cell-radius) bg-green-100 text-green-600 font-semibold data-[selected=true]:rounded-none pointer-events-none",
+            defaultClassNames.today
+          ),
+          outside: cn(
+            "text-gray-400 aria-selected:text-muted-foreground pointer-events-none",
+            defaultClassNames.outside
+          ),
+          disabled: cn(
+            "text-muted-foreground opacity-50",
+            defaultClassNames.disabled
+          ),
+          hidden: cn("invisible", defaultClassNames.hidden),
+          ...classNames,
+        }}
+        components={{
+          Root: ({ className, rootRef, ...props }) => {
+            return (
+              <div
+                data-slot="calendar"
+                ref={rootRef}
+                className={cn(className)}
+                {...props}
+              />
+            )
+          },
+          Chevron: ({ orientation, className }) => {
+            if (orientation === "left") {
+              return (
+                <Image
+                  src={ArrowLeft}
+                  alt="Previous month"
+                  width={24}
+                  height={24}
+                  className={className}
+                />
+              )
+            }
+          
+            if (orientation === "right") {
+              return (
+                <Image
+                  src={ArrowRight}
+                  alt="Next month"
+                  width={24}
+                  height={24}
+                  className={className}
+                />
+              )
+            }
+          
+            return (
+              <Image
+                src={ArrowDown}
+                alt="Open select"
+                width={24}
+                height={24}
+                className={className}
+              />
+            )
+          },
+          DayButton: ({ ...props }) => (
+            <CalendarDayButton locale={locale} {...props} />
+          ),
+          WeekNumber: ({ children, ...props }) => {
+            return (
+              <td {...props}>
+                <div className="flex size-(--cell-size) items-center justify-center text-center">
+                  {children}
+                </div>
+              </td>
+            )
+          },
+          ...components,
+        }}
+        {...props}
+      />
+    </div>
+  )
+}
+
+function CalendarDayButton({
+  className,
+  day,
+  modifiers,
+  locale,
+  ...props
+}: React.ComponentProps<typeof DayButton> & { locale?: Partial<Locale> }) {
+  const defaultClassNames = getDefaultClassNames()
+
+  const ref = React.useRef<HTMLButtonElement>(null)
+  React.useEffect(() => {
+    if (modifiers.focused) ref.current?.focus()
+  }, [modifiers.focused])
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      data-day={day.date.toLocaleDateString(locale?.code)}
+      data-selected-single={
+        modifiers.selected &&
+        !modifiers.range_start &&
+        !modifiers.range_end &&
+        !modifiers.range_middle
+      }
+      data-range-start={modifiers.range_start}
+      data-range-end={modifiers.range_end}
+      data-range-middle={modifiers.range_middle}
+      className={cn(
+        "relative isolate cursor-pointer z-10 flex aspect-square size-auto box-border w-9 min-w-(--cell-size) flex-col gap-1 border-0 leading-none data-[range-end=true]:rounded-(--cell-radius) data-[range-end=true]:rounded-r-(--cell-radius) data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground data-[range-middle=true]:rounded-none data-[range-middle=true]:bg-muted data[-range-middle=true]:text-foreground data-[range-start=true]:rounded-(--cell-radius) data-[range-start=true]:rounded-l-(--cell-radius) data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[selected-single=true]:text-green-600 data-[selected-single=true]:font-medium hover:bg-green-100 [&>span]:text-xs [&>span]:opacity-70",
+        defaultClassNames.day,
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { CalendarCommon, CalendarDayButton }
---
+++ src/components/common/EmptyDataCommon.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Image from "next/image"
+import emptyImage from "@/assets/img/empty/img-empty.jpg"
+
+type EmptyStateVariant = 'meeting' | 'lank' | 'lounge' | 'myMeeting' | 'myCreatMeeting';
+
+type EmptyStateProps = {
+  variant?: EmptyStateVariant;
+};
+
+const emptyText = {
+  meeting: "아직 모임이 없어요 \n 지금 바로 모임을 만들어보세요!",
+  lank: "아직 랭킹이 없어요",
+  lounge: "아직 게시물이 없어요",
+  myMeeting: "아직 신청한 모임이 없어요",
+  myCreatMeeting: "아직 내가만든 모임이 없어요",
+} as const;
+
+function EmptyDataCommon({ variant = 'meeting' } : EmptyStateProps) {
+  const text = emptyText[variant];
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="img-box">
+        <Image
+          width={121}
+          height={72}
+          src={emptyImage}
+          alt="데이터가 없습니다."
+        />
+      </div>
+
+      <p className="mt-6 text-center text-sm md:text-base text-gray-500 font-regular whitespace-pre-line">
+        {text}
+      </p>
+    </div>
+  )
+}
+
+export { EmptyDataCommon }
