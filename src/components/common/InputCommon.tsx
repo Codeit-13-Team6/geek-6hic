6dce7148366dbe5cca48fd47a0585b0af26d939f
parksanghyun <basira@naver.com>
2026-03-17 16:01:49 +0900

    유효성검사 ui 제작완료

--- src/components/common/InputCommon.tsx
+++ src/components/common/InputCommon.tsx
@@ -31,12 +31,12 @@ const inputVariants = cva(
   {
     variants: {
       isDestructive: {
-        true: "border-error aria-invalid:border-error focus:ring-[1px] focus:ring-error/20",
+        true: "border-error aria-invalid:border-error focus:ring-[1px] focus:ring-error",
         false:
           "border-gray-300 focus:border-main-green-500 focus:ring-[1px] focus:ring-green-500",
       },
       inputSize: {
-        sm: "h-10 px-3 py-2 text-sm md:h-12 md:p-3",
+        sm: "h-10 px-3 py-2 text-sm md:text-base md:h-12 md:p-3",
         lg: "h-[50px] px-4 py-3 text-base",
       },
     },
