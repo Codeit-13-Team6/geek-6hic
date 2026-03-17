1a40010fe1cbe34a3185ad04f2360e720a164779
사민주 <saminju@saminjuui-MacBookPro.local>
2026-03-17 18:25:29 +0900

    feat: 가드 범위 수정 및 meetings로 라우터 수정

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. 쿠키에서 토큰을 꺼내봅니다.
   const token = request.cookies.get("accessToken")?.value;
 
   // 2. 만약 토큰이 없는데 보호된 페이지로 가려고 한다면?
-  const protectedPaths = ["/mypage", "/lounge", "/meeting", "/users"];
+  const protectedPaths = [
+    "/lounge",
+    "/meetings",
+    "/users",
+    "/my-meetings",
+    "/ranking",
+  ];
   const isProtected = protectedPaths.some((path) =>
     request.nextUrl.pathname.startsWith(path),
   );
@@ -23,10 +29,10 @@ export function middleware(request: NextRequest) {
 // 이 미들웨어가 작동할 주소들 (불필요한 곳은 검사 안 함)
 export const config = {
   matcher: [
-    "/mypage/:path*",
     "/lounge/:path*",
-    "/meeting/:path*",
+    "/meetings/:path*",
     "/users/:path*",
-    "/dashboard/:path*",
+    "/my-meetings/:path*",
+    "/ranking/:path*",
   ],
 };
