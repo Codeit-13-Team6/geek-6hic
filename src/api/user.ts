559305a78f1f4bb4eca402e4e5cae9b20bad4119
제현 <jehyeon@jehyeon-ui-Macmini.local>
2026-03-18 16:49:49 +0900

    feat: 마이페이지 프로필 업데이트 , 기본 모달 수정, 이미지 업로드 파일 수정 api 연결 등

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,14 @@
 import axiosInstance from "@/lib/axios";
-import { User } from "@/types";
+import { Meeting, User, UserProfileUpdateProps } from "@/types";
 
 export async function getUser(): Promise<User> {
   const { data } = await axiosInstance.get("/users/me");
+  return data;
+}
+
+export async function updateUserProfile(
+  profileForm: UserProfileUpdateProps,
+): Promise<User> {
+  const { data } = await axiosInstance.patch("/users/me", profileForm);
   return data;
 }
