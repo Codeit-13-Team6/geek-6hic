6d6b9e2bdfd95f3cd4e55e93b0e67dd151f67fd8
제현 <jehyeon@jehyeon-ui-Macmini.local>
2026-03-18 10:34:04 +0900

    feat: 스프린트 라운지 게시물 영역 제작

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -14,3 +14,11 @@ export async function getPosts(params?: GetPostsParams): Promise<Posts[]> {
   const { data } = await axiosInstance.get("/posts", { params });
   return data.data;
 }
+
+
+
+
+export async function getPostsDetail(postId: number): Promise<Posts[]> {
+  const { data } = await axiosInstance.get(`/posts${postId}`);
+  return data.data;
+}
