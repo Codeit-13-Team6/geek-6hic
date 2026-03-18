52ae56719911417d95db539d56ded932bae8b102
sang7085 <54009751+sang7085@users.noreply.github.com>
2026-03-18 14:43:24 +0900

    [FEAT] user.data 새로고침 시 새로 받아오는 기능 (#79)
    * 최신화 merge
    
    * 유저조회용 route 완료
    
    * 스태쉬용
    
    * refacter/폴더구조변경 merge
    
    * feat/user.data 새로고침시 제작완료

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,11 +1,21 @@
 import { create } from "zustand";
+import type { User } from "@/types/index";
 
 interface AuthState {
-  userId: number | null;
-  setUserId: (id: number) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
+  clearAuth: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-  userId: null,
-  setUserId: (id) => set({ userId: id }),
+  user: null,
+  setUser: (user) => set({ user }),
+  clearAuth: () => set({ user: null }),
 }));
+
+// 페이지에서 사용 예시
+// import { useAuthStore } from '@/store/useAuthStore';
+// const user = useAuthStore((s) => s.user);
+// const clearAuth = useAuthStore((s) => s.clearAuth);
+// console.log(user);
+// console.log(user?.id);
