03c98fb8e545e0eba77a1c6fe437e98cc58a678b
사민주 <saminju@saminjuui-MacBookPro.local>
2026-03-19 17:19:21 +0900

    style:수정

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -75,6 +75,7 @@ export default function SignUp() {
             >
               회원가입
             </h1>
+
             <form
               onSubmit={handleSubmit(onSubmit)}
               noValidate
