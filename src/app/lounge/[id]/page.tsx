7d6611d30fda6d50987403aa1f6b328b77367b1c
dbwlsyyy <dbwlsyyy@naver.com>
2026-03-24 18:49:13 +0900

    feat: 삭제 confirm 모달 적용

--- src/app/lounge/[id]/page.tsx
+++ src/app/lounge/[id]/page.tsx
@@ -10,12 +10,16 @@ import {
   useGetPostDetail,
   useToggleLike,
 } from "@/hooks/queries/usePosts";
+import { useState } from "react";
+import ModalBase from "@/components/ui/ModalBase";
+import { BtnCommon } from "@/components/ui/BtnCommon";
 
 export default function LoungeDetailPage() {
   const { id } = useParams();
   const router = useRouter();
   const postId = Number(id);
   const userId = useAuthStore((state) => state.user?.id);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const { data: post, isLoading, isError } = useGetPostDetail(postId);
   const { mutate: removePost } = useDeletePost(postId);
@@ -27,11 +31,13 @@ export default function LoungeDetailPage() {
   );
 
   const handlePostDelete = () => {
-    if (confirm("정말 이 게시글을 삭제하시겠습니까? (복구할 수 없습니다)")) {
-      removePost();
-    }
+    setIsDeleteModalOpen(true);
   };
 
+  const handleConfirmDelete = () => {
+    removePost();
+    setIsDeleteModalOpen(false);
+  };
   const handlePostEdit = () => {
     router.push(`/lounge/edit/${postId}`);
   };
@@ -73,6 +79,37 @@ export default function LoungeDetailPage() {
 
         <CommentSection postId={postId} />
       </div>
+
+      <ModalBase
+        isOpen={isDeleteModalOpen}
+        onOpenChange={setIsDeleteModalOpen}
+        title="게시글 삭제"
+      >
+        <div className="flex flex-col gap-6 pt-4">
+          <p className="leading-relaxed text-gray-700">
+            게시글을 삭제하시겠습니까? <br />
+            삭제된 게시글은 복구할 수 없습니다.
+          </p>
+
+          <div className="flex justify-end gap-2">
+            <BtnCommon
+              variant="teritary"
+              onClick={() => setIsDeleteModalOpen(false)}
+              size="sm"
+              className="w-[60px]"
+            >
+              취소
+            </BtnCommon>
+            <BtnCommon
+              onClick={handleConfirmDelete}
+              size="sm"
+              className="w-[60px]"
+            >
+              삭제
+            </BtnCommon>
+          </div>
+        </div>
+      </ModalBase>
     </div>
   );
 }
