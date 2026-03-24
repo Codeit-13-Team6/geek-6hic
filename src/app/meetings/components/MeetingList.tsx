23e7019baafc4d56df6d7f78e4e6eb7b324011d9
sang7085 <54009751+sang7085@users.noreply.github.com>
2026-03-24 15:39:22 +0900

    feat: 모임찾기UI 재사용 api사용고려한 로직 (#147)

--- src/app/meetings/components/MeetingList.tsx
+++ src/app/meetings/components/MeetingList.tsx
@@ -1,160 +1,151 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import demoImage from "@/assets/img/mock/banner-lg-demo.jpg";
-import alram from "@/assets/icon/alarm/alarm-blue.svg";
-import heartOn from "@/assets/icon/hearts/hearts-true.svg";
-import heartOff from "@/assets/icon/hearts/hearts-false.svg";
-import person from "@/assets/icon/person/person.svg";
-import { Progress } from "@/components/ui/ProgressCommon";
+import Image from 'next/image';
+import demoImage from '@/assets/img/mock/banner-lg-demo.jpg';
+import alram from '@/assets/icon/alarm/alarm-blue.svg';
+import heartOff from '@/assets/icon/hearts/hearts-false.svg';
+import person from '@/assets/icon/person/person.svg';
+import { Progress } from '@/components/ui/ProgressCommon';
+import type { Meeting } from '@/types';
 
-export default function MeetingList() {
+interface MeetingListProps {
+  meetingList: Meeting[];
+  isLoading: boolean;
+  onItemClick: (item: Meeting) => void;
+}
 
-  // 마감됐을 때 dimd 조건 1 마감시간 지났을 때, 조건 2 인원 다 찼을 때(임시로직)
-  function isMeetingClosed(meeting: {
-    dateTime: string;
-    participantCount: number;
-    capacity: number;
-  }) {
+export default function MeetingList({
+  meetingList,
+  isLoading,
+  onItemClick,
+}: MeetingListProps) {
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  // 마감날짜 계산기
+  function getDeadlineLabel(registrationEnd: string) {
+    const endDate = new Date(registrationEnd);
     const now = new Date();
-    const isExpired = new Date(meeting.dateTime) < now;
-    const isFull = meeting.participantCount >= meeting.capacity;
+
+    const isSameYear = endDate.getFullYear() === now.getFullYear();
+    const isSameMonth = endDate.getMonth() === now.getMonth();
+    const isSameDate = endDate.getDate() === now.getDate();
+
+    const isToday = isSameYear && isSameMonth && isSameDate;
+
+    if (!isToday) return null;
+
+    const hours = String(endDate.getHours()).padStart(2, '0');
+
+    return `오늘 ${hours}시 마감`;
+  }
+
+  // 날짜
+  function formatDate(dateTime: string) {
+    const date = new Date(dateTime);
+  
+    return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+  }
+
+  // 시간
+  function formatTime(dateTime: string) {
+    const date = new Date(dateTime);
+  
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+    return `${hours}:${minutes}`;
+  }
+
+  function isMeetingClosed(item: Meeting) {
+    const now = new Date();
+    const isExpired = new Date(item.dateTime) < now;
+    const isFull = item.participantCount >= item.capacity;
 
     return isExpired || isFull;
   }
 
-  const mockMeetingList = [
-    {
-      id: 1,
-      title: '건대 러닝 모임',
-      subTitle: '스터디',
-      type: '달램핏',
-      region: '건대입구',
-      dateTime: '2026-03-25T10:00:00',
-      participantCount: 20,
-      capacity: 20,
-      image: demoImage,
-      tags: {
-        date: '1월 7일',
-        time: '17:30',
-        deadline: '오늘 21시 마감',
-      },
-    },
-    {
-      id: 2,
-      title: '강남 헬스 번개',
-      subTitle: '스터디',
-      type: '달램핏',
-      region: '강남',
-      dateTime: '2026-03-26T19:00:00',
-      participantCount: 8,
-      capacity: 10,
-      image: demoImage,
-      tags: {
-        date: '1월 7일',
-        time: '17:30',
-        deadline: '오늘 21시 마감',
-      }
-    },
-    {
-      id: 3,
-      title: '홍대 요가 클래스',
-      subTitle: '스터디',
-      type: '달램핏',
-      region: '홍대',
-      dateTime: '2026-03-27T09:00:00',
-      participantCount: 3,
-      capacity: 8,
-      image: demoImage,
-      tags: {
-        date: '1월 7일',
-        time: '17:30',
-        deadline: '오늘 21시 마감',
-      }
-    },
-    {
-      id: 4,
-      title: '잠실 클라이밍 모임',
-      subTitle: '스터디',
-      type: '달램핏',
-      region: '잠실',
-      dateTime: '2026-03-28T14:00:00',
-      participantCount: 10,
-      capacity: 10,
-      image: demoImage,
-      tags: {
-        date: '1월 7일',
-        time: '17:30',
-        deadline: '오늘 21시 마감',
-      }
-    },
-  ];
-
   return (
     <>
-      {mockMeetingList.map((meeting) => {
-        const isClosed = isMeetingClosed(meeting);
+      {meetingList.map((item) => {
+        const isClosed = isMeetingClosed(item);
+        const deadLine = getDeadlineLabel(item.registrationEnd);
+
         return (
-          <div key={meeting.id} className="relative overflow-hidden rounded-3xl sm:flex sm:items-center sm:gap-5 sm:p-6 sm:bg-white sm:rounded-[32px] cursor-pointer">
-            <button className="absolute top-4 right-4 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border border-gray-200 bg-white">
-              <div className="relative h-6 w-6">
-                <Image src={heartOff} fill alt="찜" />
-                {/* <Image src={heartOn} fill alt="찜" /> */}
+          <div
+            key={item.id}
+            onClick={() => onItemClick(item)
+          }
+            
+            className='relative cursor-pointer overflow-hidden rounded-3xl sm:flex sm:items-center sm:gap-5 sm:rounded-[32px] sm:bg-white sm:p-6'
+          >
+            <button
+              type='button'
+              className='absolute top-4 right-4 z-10 flex h-10 w-10 items-center justify-center rounded-full border border-gray-200 bg-white cursor-pointer'
+            >
+              <div className='relative h-6 w-6'>
+                <Image src={heartOff} fill alt='찜' />
               </div>
             </button>
 
-            <div className="relative h-39 w-full sm:w-[170px] sm:h-[170px] sm:rounded-3xl overflow-hidden">
+            <div className='relative h-39 w-full overflow-hidden sm:h-[170px] sm:w-[170px] sm:rounded-3xl'>
               <Image
-                src={meeting.image}
+                src={item.image || demoImage}
                 fill
-                alt="게시물 이미지"
+                alt='게시물 이미지'
+                unoptimized
               />
 
               {isClosed && (
-                <div className="absolute w-full h-full flex items-center justify-center bg-black/70">
-                  <span className="font-[Tenada] rounded-full text-2xl font-semibold text-white">
+                <div className='absolute flex h-full w-full items-center justify-center bg-black/70'>
+                  <span className='font-[Tenada] rounded-full text-2xl font-semibold text-white'>
                     모집 마감
                   </span>
                 </div>
               )}
             </div>
 
-            <div className="flex flex-col bg-white p-4 sm:p-0 flex-1">
-              <div className="flex flex-col">
-                <h3 className="text-xl font-semibold text-black">{meeting.title}</h3>
-                <h4 className="text-sm font-semibold text-gray-500">{meeting.subTitle}</h4>
+            <div className='flex flex-1 flex-col bg-white p-4 sm:p-0'>
+              <div className='flex flex-col'>
+                <h3 className='text-xl font-semibold text-black'>{item.name}</h3>
+                <h4 className='text-sm font-semibold text-gray-500'>{item.type}</h4>
               </div>
 
-              <div className="flex gap-2 mt-[14px] sm:mt-10 flex-wrap">
-                <span className="rounded-lg border border-gray-200 px-2 py-0.5 text-sm text-gray-600">
-                  {meeting.tags.date}
+              <div className='mt-[14px] flex flex-wrap gap-2 sm:mt-10'>
+                <span className='rounded-lg border border-gray-200 px-2 py-0.5 text-sm text-gray-600'>
+                  {formatDate(item.dateTime)}
                 </span>
-                <span className="rounded-lg border border-gray-200 px-2 py-0.5 text-sm text-gray-600">
-                  {meeting.tags.time}
+                <span className='rounded-lg border border-gray-200 px-2 py-0.5 text-sm text-gray-600'>
+                  {formatTime(item.dateTime)}
                 </span>
-                <span className="flex items-center gap-1 rounded-lg bg-[rgba(24,220,255,0.2)] px-2 py-0.5">
-                  <span className="relative h-6 w-6">
-                    <Image src={alram} fill alt="알람 아이콘" />
+                {deadLine
+                  ?
+                  <span className='flex items-center gap-1 rounded-lg bg-[rgba(24,220,255,0.2)] px-2 py-0.5'>
+                    <span className='relative h-6 w-6'>
+                      <Image src={alram} fill alt='알람 아이콘' />
+                    </span>
+                    <span className='text-sm font-semibold text-blue-600'>
+                      {deadLine}
+                    </span>
                   </span>
-                  <span className="text-sm font-semibold text-blue-600">
-                    {meeting.tags.deadline}
-                  </span>
-                </span>
+                  :
+                  null
+                }
               </div>
 
-              <div className="mt-5 flex items-center w-full">
-                <div className="relative h-4 w-4 shrink-0">
-                  <Image src={person} fill alt="인원" />
+              <div className='mt-5 flex w-full items-center'>
+                <div className='relative h-4 w-4 shrink-0'>
+                  <Image src={person} fill alt='인원' />
                 </div>
                 <Progress
-                  className="ml-[5px] w-full"
-                  value={(meeting.participantCount / meeting.capacity) * 100}
+                  className='ml-[5px] w-full'
+                  value={(item.participantCount / item.capacity) * 100}
                 />
-                <p className="ml-[13px]">
-                  <span className="text-sm font-semibold text-green-500">
-                    {meeting.participantCount}
+                <p className='ml-[13px]'>
+                  <span className='text-sm font-semibold text-green-500'>
+                    {item.participantCount}
                   </span>
-                  <span className="text-sm text-gray-600">/{meeting.capacity}</span>
+                  <span className='text-sm text-gray-600'>/{item.capacity}</span>
                 </p>
               </div>
             </div>
