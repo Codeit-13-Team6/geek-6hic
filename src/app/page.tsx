80f4cb1b8a7d6e0c2173d71604e99d9829f6fb27
사민주 <saminju@saminjuui-MacBookPro.local>
2026-03-26 18:53:00 +0900

    feat: 레이아웃 구축

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { CreateMeetingModal } from "@/app/meetings/modal/CreateMeetingModal";
+import Notification from "@/components/layout/Notification";
 
 export default function Home() {
   return (
     <>
       <CreateMeetingModal />
+      <Notification />
     </>
   );
 }
