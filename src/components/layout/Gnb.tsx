09bb0565c30ac1fd62777690aa266d0c5587f32b
사민주 <saminju@saminjuui-MacBookPro.local>
2026-03-27 15:08:16 +0900

    style: 필요없는거 삭제

--- src/components/layout/Gnb.tsx
+++ src/components/layout/Gnb.tsx
@@ -6,7 +6,6 @@ import axios from "axios";
 import { useRouter, usePathname } from "next/navigation";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import bellIconSm from "@/assets/icon/bells/bell-default-sm-false.svg";
 import bellIconLg from "@/assets/icon/bells/bell-default-lg-false.svg";
 import menu from "@/assets/icon/menu/menu.svg";
 import logoSm from "@/assets/img/logo/logo-sm.jpg";
@@ -75,15 +74,15 @@ export function Gnb() {
           <nav className="hidden items-center lg:flex lg:gap-2">
             {NAV_LINKS.map((link) => (
               <Link
-              key={link.name}
-              href={link.href}
-              className={cn(
-                "font-pretendard hover:text-main-green-600 font-medium whitespace-nowrap transition-all hover:font-semibold sm:px-2 sm:py-4 sm:text-base lg:px-4",
-                pathname === link.href
-                  ? "text-green-600 font-semibold"
-                  : "text-slate-600"
-              )}
-            >
+                key={link.name}
+                href={link.href}
+                className={cn(
+                  "font-pretendard hover:text-main-green-600 font-medium whitespace-nowrap transition-all hover:font-semibold sm:px-2 sm:py-4 sm:text-base lg:px-4",
+                  pathname === link.href
+                    ? "font-semibold text-green-600"
+                    : "text-slate-600",
+                )}
+              >
                 {link.name}
               </Link>
             ))}
