8f60c74867d4ee94b5e3beefd9eef1abb92cf6ce
parksanghyun <basira@naver.com>
2026-04-06 16:42:05 +0900

    bug: 로그인, 회원가입 높이값 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({
 
             <ToasterProvider />
             <LoginModalProvider />
-            <main className="h-[calc(100dvh-72px)]">
+            <main className="min-h-[calc(100dvh-72px)]">
             {/* <main className="mx-auto w-full max-w-[1200px] px-6 py-8 sm:px-8 lg:py-20"> */}
               {children}
             </main>
