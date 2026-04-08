7f79bc3cd484418072bf62c49034505079ea95ee
dbwlsyyy <dbwlsyyy@naver.com>
2026-04-08 14:15:38 +0900

    ui: 코드 스니펫 하이라이팅 적용

--- src/app/lounge/[id]/component/CodeBlock.tsx
+++ src/app/lounge/[id]/component/CodeBlock.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import {
   atomOneDark,
   atomOneLight,
