2a93050c7197a1e08af0bc6aa89edb9e58f5992d
사민주 <saminju@saminjuui-MacBookPro.local>
2026-04-14 18:10:36 +0900

    fix:환경 변수 수정

--- src/app/api/oauth/kakao/route.ts
+++ src/app/api/oauth/kakao/route.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
 const KAKAO_CLIENT_SECRET = process.env.KAKAO_CLIENT_SECRET;
 
 type OAuthLoginResult = {
