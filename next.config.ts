491634404c677443dbd02f6ce9441c7f74773785
parksanghyun <basira@naver.com>
2026-04-14 22:14:14 +0900

    feat: aboutTeam 작업완료

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,7 @@ const nextConfig: NextConfig = {
       "picsum.photos",
       "via.placeholder.com",
       "avatar.vercel.sh",
+      "img1.kakaocdn.net",
     ],
     remotePatterns: [
       {
