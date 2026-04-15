9dc6544b46cd002bebcf195269b7014393071121
XeXyeon <132742954+XeXyeon@users.noreply.github.com>
2026-04-15 10:20:13 +0900

    fix: gnb 버그 수정 (#564)
    Co-authored-by: 제현 <jehyeon@jehyeon-ui-Macmini.local>

--- src/providers/MemberProvider.tsx
+++ src/providers/MemberProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useAuthStore } from "@/store/useAuthStore";
 import { MemberProviderProps } from "@/types";
 import type { User } from "@/types";
@@ -10,11 +10,15 @@ interface Props extends MemberProviderProps {
 }
 
 export function MemberProvider({ children, initialUser }: Props) {
-  const initialized = useRef(false);
-  if (!initialized.current && initialUser) {
-    useAuthStore.setState({ user: initialUser as User, isAuthLoading: false });
-    initialized.current = true;
-  }
+  // const initialized = useRef(false);
+  useEffect(() => {
+    if (initialUser) {
+      useAuthStore.setState({
+        user: initialUser as User,
+        isAuthLoading: false,
+      });
+    }
+  }, [initialUser?.id, initialUser?.name, initialUser?.image]);
 
   return <>{children}</>;
 }
