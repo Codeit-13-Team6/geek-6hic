62724e73bf3fc982942a2e223524eb40e9c8cd93
사민주 <saminju@saminjuui-MacBookPro.local>
2026-04-15 10:48:52 +0900

    chore:테스트 기초세팅

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  //무한 스크롤 위해 필요
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
